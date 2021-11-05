4cff69db4b4dd2d2a462aee3148e565544a9ef5a
martinezga <gabriellamartinezv@gmail.com>
2021-11-05 15:23:41 -0300

    started django app

---
+++ backend/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting backend..."
+
+# # python manage.py flush --no-input
+# python manage.py makemigrations || {
+#     echo "Failed to run makemigrations."
+#     exit 1
+# }
+
+# # Apply migrations.
+# python manage.py migrate || {
+#     echo "Failed to run db migrations."
+#     exit 1
+# }
+
+# python manage.py collectstatic --no-input --clear || {
+#      echo "Failed to collect static files."
+#      exit 1
+# }
+
+exec "$@"
