4e12cb61242f8ab39aaa48107fabb5238746a8cd
martinezga <52822208+martinezga@users.noreply.github.com>
2021-11-27 19:44:36 -0300

    changed home url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from api.views import MyApiView
+from api.apps import ApiConfig
 
 
+app_name = ApiConfig.name
 urlpatterns = [
     # example: /api/v1/
     path('', MyApiView.as_view(), name='api-index'),
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.apps import ApiConfig
 
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('api/v1/', include('api.urls')),
+    path('api/v1/', include('api.urls', namespace=ApiConfig.name)),
 ]
